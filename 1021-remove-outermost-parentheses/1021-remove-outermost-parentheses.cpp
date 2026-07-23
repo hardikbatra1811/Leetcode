01af2393075fd7d99bc42c46f607ea3638b1f1e5
Hardik Batra <hardikbatra20@gmail.com>
2026-07-23 09:31:12 +0530

    Time: 0 ms (100%), Space: 9.1 MB (13.59%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+string removeOuterParentheses(string s) {
+    string result="";
+    stack<char> st;
+    for(int i =0; i<s.size(); i++){
+        if(s[i] == '('){
+            st.push(s[i]);
+            if(st.size()>1){
+                result+=s[i];
+            }
+            }else{
+                if(!st.empty() && st.top() == '('){
+                    st.pop();
+                    if(st.size() >=1)result+=')';
+                }
+            }
+        }
+        return result;
+    }
+};
