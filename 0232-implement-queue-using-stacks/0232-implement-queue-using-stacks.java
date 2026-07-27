6d6077f23cfa3cb745ccb982da38e622f1c020e1
Hardik Batra <hardikbatra20@gmail.com>
2026-07-27 11:36:51 +0530

    Time: 0 ms (100%), Space: 42.8 MB (51.62%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,37 @@
+
+
+class MyQueue {
+    private Stack<Integer> stk1;
+    private Stack<Integer> stk2;
+
+    public MyQueue() {
+        stk1 = new Stack<>();
+        stk2 = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stk1.push(x);
+    }
+    
+    public int pop() {
+        shiftStacks();
+        return stk2.pop();
+    }
+    
+    public int peek() {
+        shiftStacks();
+        return stk2.peek();
+    }
+    
+    public boolean empty() {
+        return stk1.isEmpty() && stk2.isEmpty();
+    }
+
+    private void shiftStacks() {
+        if (stk2.isEmpty()) {
+            while (!stk1.isEmpty()) {
+                stk2.push(stk1.pop());
+            }
+        }
+    }
+}
